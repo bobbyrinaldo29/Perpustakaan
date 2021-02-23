931fc96eb718913fd0a51fbb59c0273f39782321
bobbyrinaldo29 <49853622+bobbyrinaldo29@users.noreply.github.com>
2021-02-23 08:50:38 +0700

    First Commit
    Add file and folder from htdoc

---
+++ application/views/global/profile.php
@@ -0,0 +1,99 @@
+<div class="row">
+                <div class="col-md-12">
+                   <center> <h4 class="page-head-line">Profile SMK Widya Dirgantara</h4></center>
+                </div>
+
+<div class="row">
+                <div class="col-md-6">
+                      <div class="notice-board">
+                        <div class="panel panel-info">
+                        <div class="panel-heading">
+                        <i class=" glyphicon glyphicon-picture"></i>
+                            Photo Profile SMK Widya Dirgantara
+                        </div>
+                                <div class="panel-body">
+
+                               <img src="<?php echo base_url(); ?>assets2/img/WD.jpg" width="265" height="240">&nbsp
+                               <img src="<?php echo base_url(); ?>assets2/img/wd1.jpg" width="265" height="240">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                <div class="col-md-6">
+                      <div class="notice-board">
+                        <div class="panel panel-info">
+                        <div class="panel-heading">
+                        <class="box-title"><i class=" glyphicon glyphicon-home"></i>
+                            Identitas SMK Widya Dirgantara 
+                        </div>
+                                <div class="panel-body">
+                                <table width="100%">
+                                <tr>
+                                    <td width="60%">
+                                        <b>Alamat :</b> JL.BOJONG RAYA 114 BANDUNG<br>
+                                        <b>NPSN :</b> 20265547<br>
+                                        <b>Status :</b> Swasta<br>
+                                        <b>Akreditasi :</b> A<br>
+                                        <b>Bentuk Pendidikan :</b> SMK<br>
+                                        <b>Status Kepemilikan :</b> Yayasan<br>
+                                        <b>SK Pendirian Sekolah :</b> 047/102/KEP/E87<br>
+                                        <b>Tanggal SK Izin Operasional :</b> 1987-01-30<br>
+                                    </td>
+                                    <td width="40%">
+                                        <b>Tanggal SK Pendirian :</b> 1987-01-30<br>
+                                        <b>Luas Tanah Milik :</b> 1469<br> 
+                                        <b>SK Izin Operasional :</b> 047/102/KEP/E87<br><br><br><br><br><br>
+                                    </td>
+                                 </tr>
+                            </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                      <div class="notice-board">
+                        <div class="panel panel-info">
+                        <div class="panel-heading">
+                        <class="box-title"><i class=" glyphicon glyphicon-th-list"></i>
+                            MISI SMK Pembangunan 
+                        </div>
+                                <div class="panel-body">
+                                <ul align="justify">
+                                1. Menanamkan keimanan dan ketaqwaan melalui pengamalan ajaran agama
+                                <br>2. Meningkatkan profesionalisme pendidik dan tenaga kependidikan.
+                                <br>3. Menerapkan dan menjunjung tinggi budaya nasional dan budaya lokal serta berbudi pekerti luhur.
+                                <br>4. Mengembangkan bidang ilmu pengetahuan dan teknologi berdasarkan minat, bakat dan potensi peserta didik.
+                                <br>5. Menjalin kerjasama yang harmonis antarwarga sekolah, dengan masyarakat, DU/DI dan lembaga lain yang terkait.
+                                <br>6. Menciptakan lingkungan sekolah yang asri, nyaman dan sehat.
+                               </ul>  
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                      <div class="notice-board">
+                        <div class="panel panel-info">
+                        <div class="panel-heading">
+                        <class="box-title"><i class=" glyphicon glyphicon-th-list"></i>
+                            VISI SMK Pembangunan
+                        </div>
+                                <div class="panel-body">
+                                <ul align="justify">
+                                “Mewujudkan lembaga pendidikan yang bermutu didukung oleh sumber daya manusia (SDM) 
+                                yang profesional, beriman, berbudaya, dan berwawasan lingkungan sehingga menghasilkan 
+                                lulusan yang kompetitif dan berwawasan global”.
+                               </ul>  
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
